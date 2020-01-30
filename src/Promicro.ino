bf967eba9c8709689e36fe9253897286e1212667
4dvn <vuha.hau@hyper.vn>
2020-01-31 01:50:58 +0700

    Update Promicro.ino

--- src/Promicro.ino
+++ src/Promicro.ino
@@ -6,7 +6,7 @@ USBMIDI_Interface midi;
 
 // Instantiate a multiplexer
 CD74HC4067 mux = {
-  A0,              // analog pin
+  A1,              // analog pin
   {0, 1, 2, 3}, // Address pins S0, S1, S2, S3
   // 7, // Optionally, specify the enable pin
 };
@@ -25,18 +25,18 @@ CCPotentiometer volumePotentiometers[] = {
   { mux.pin(1), { MIDI_CC::Channel_Volume, CHANNEL_2 } },
   { mux.pin(2), { MIDI_CC::Channel_Volume, CHANNEL_3 } },
   { mux.pin(3), { MIDI_CC::Channel_Volume, CHANNEL_4 } },
-  { mux.pin(4), { MIDI_CC::Channel_Volume, CHANNEL_5 } },
-  { mux.pin(5), { MIDI_CC::Channel_Volume, CHANNEL_6 } },
-  { mux.pin(6), { MIDI_CC::Channel_Volume, CHANNEL_7 } },
-  { mux.pin(7), { MIDI_CC::Channel_Volume, CHANNEL_8 } },
-  { mux.pin(8), { MIDI_CC::Channel_Volume, CHANNEL_9 } },
-  { mux.pin(9), { MIDI_CC::Channel_Volume, CHANNEL_10 } },
-  { mux.pin(10), { MIDI_CC::Channel_Volume, CHANNEL_11 } },
-  { mux.pin(11), { MIDI_CC::Channel_Volume, CHANNEL_12 } },
-  { mux.pin(12), { MIDI_CC::Channel_Volume, CHANNEL_13 } },
-  { mux.pin(13), { MIDI_CC::Channel_Volume, CHANNEL_14 } },
-  { mux.pin(14), { MIDI_CC::Channel_Volume, CHANNEL_15 } },
-  { mux.pin(15), { MIDI_CC::Channel_Volume, CHANNEL_16 } },
+  //{ mux.pin(4), { MIDI_CC::Channel_Volume, CHANNEL_5 } },
+  //{ mux.pin(5), { MIDI_CC::Channel_Volume, CHANNEL_6 } },
+  //{ mux.pin(6), { MIDI_CC::Channel_Volume, CHANNEL_7 } },
+  //{ mux.pin(7), { MIDI_CC::Channel_Volume, CHANNEL_8 } },
+  //{ mux.pin(8), { MIDI_CC::Channel_Volume, CHANNEL_9 } },
+  //{ mux.pin(9), { MIDI_CC::Channel_Volume, CHANNEL_10 } },
+  //{ mux.pin(10), { MIDI_CC::Channel_Volume, CHANNEL_11 } },
+  //{ mux.pin(11), { MIDI_CC::Channel_Volume, CHANNEL_12 } },
+  //{ mux.pin(12), { MIDI_CC::Channel_Volume, CHANNEL_13 } },
+  //{ mux.pin(13), { MIDI_CC::Channel_Volume, CHANNEL_14 } },
+  //{ mux.pin(14), { MIDI_CC::Channel_Volume, CHANNEL_15 } },
+  //{ mux.pin(15), { MIDI_CC::Channel_Volume, CHANNEL_16 } },
 };
 
 
